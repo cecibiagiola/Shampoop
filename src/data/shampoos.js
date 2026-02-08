5209c1bd0e5c14f8b86fdef165af1b5576d706ec
Cecilia <biagiolacecilia@gmail.com>
2026-02-08 20:07:13 -0300

    Image format fixed

--- src/data/shampoos.js
+++ src/data/shampoos.js
@@ -2,17 +2,17 @@ const shampoos = [
   {
     id: 1,
     name: "Sedal Ceramidas",
-    image: "/images/Sedal-restauracion.jpg",
+    image: "/images/Sedal-restauracion.png",
   },
   {
     id: 2,
     name: "Pantene Pro-V",
-    image: "/images/Sedal-ceramidas.jpg",
+    image: "/images/Sedal-ceramidas.png",
   },
   {
     id: 3,
     name: "Elvive Reparación Total",
-    image: "/images/Sedal-AcidoHialuronico.jpg",
+    image: "/images/Sedal-AcidoHialuronico.png",
   },
 ];
 
